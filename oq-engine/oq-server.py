c1dbccab73fd3d5b23e4b94b5d8ae4cedebdf239
Daniele Viganò <daniele@vigano.me>
2016-05-18 14:59:39 +0200

    Start django webserver internally

--- oq-engine/oq-server.py
+++ oq-engine/oq-server.py
@@ -4,8 +4,8 @@ import os
 scriptdir, script = os.path.split(__file__)
 pkgdir = os.path.join(scriptdir, 'pkgs')
 sys.path.insert(0, pkgdir)
-os.environ['PYTHONPATH'] = (pkgdir + os.pathsep
-                            + os.environ.get('PYTHONPATH', ''))
+os.environ['PYTHONPATH'] = (pkgdir + os.pathsep +
+                            os.environ.get('PYTHONPATH', ''))
 
 # APPDATA should always be set, but in case it isn't, try user home
 # If none of APPDATA, HOME, USERPROFILE or HOMEPATH are set, this will fail.
@@ -34,31 +34,37 @@ from openquake.engine.logs import dbcmd
 
 def _help():
     print("Usage: %s <dbserver|webui> <start|stop>" % sys.argv[0])
-
+    sys.exit(1)
 
 if __name__ == '__main__':
-    if sys.argv[1] == 'dbserver':
-        if sys.argv[2] == 'start':
-            runserver()
-        elif sys.argv[2] == 'stop':
-            dbcmd('stop')
-        else:
-            _help()
-    elif sys.argv[1] == 'webui':
-        if sys.argv[2] == 'start':
-            from django.core.management import execute_from_command_line
-            from openquake.server import executor
-            from openquake.engine import logs
 
-            os.environ.setdefault(
-                "DJANGO_SETTINGS_MODULE", "openquake.server.settings")
-            # check the database version
-            logs.dbcmd('check_outdated')
-            # reset is_running
-            logs.dbcmd('reset_is_running')
-            with executor:
-                execute_from_command_line(sys.argv)
+    if len(sys.argv) < 3:
+        _help()
+    else:
+        if sys.argv[1] == 'dbserver':
+            if sys.argv[2] == 'start':
+                runserver()
+            elif sys.argv[2] == 'stop':
+                dbcmd('stop')
+            else:
+                _help()
+        elif sys.argv[1] == 'webui':
+            if sys.argv[2] == 'start':
+                from django.core.management import call_command
+                from django.core.wsgi import get_wsgi_application
+                from openquake.server import executor
+                from openquake.engine import logs
+
+                os.environ.setdefault(
+                    "DJANGO_SETTINGS_MODULE", "openquake.server.settings")
+                # check the database version
+                logs.dbcmd('check_outdated')
+                # reset is_running
+                logs.dbcmd('reset_is_running')
+                with executor:
+                    application = get_wsgi_application()
+                    call_command('runserver',  '127.0.0.1:8000')
+            else:
+                _help()
         else:
             _help()
-    else:
-        _help()
