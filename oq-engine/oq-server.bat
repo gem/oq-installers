1a8d4d070c3f19e776092a95f52988da6daca183
Daniele Viganò <daniele@vigano.me>
2016-05-19 10:59:45 +0200

    Windows sucks

--- oq-engine/oq-server.bat
+++ oq-engine/oq-server.bat
@@ -10,8 +10,15 @@ if "%PROCESSOR_ARCHITECTURE%"=="x86" (
 
 REM Start the DbServer in background but within the same context
 start "OpenQuake DB server" /B "%COMMONPROGRAMFILES%\Python\2.7\python.exe" -m openquake.server.dbserver
+
 REM Make sure that the dbserver is up and running
-timeout /t 10 /nobreak > NUL
+if exist C:\Windows\System32\timeout.exe (
+    timeout /t 10 /nobreak > NUL
+) else (
+    REM Windows XP hack
+    ping 192.0.2.2 -n 1 -w 10000 > nul 
+)
+
 REM Start the WebUI using django
 "%common%\Python\2.7\python.exe" -m openquake.server.manage runserver %*
 
