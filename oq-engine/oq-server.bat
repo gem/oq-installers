ae7e270c5ab5b2548787f0b71a8708b6a05fdbb3
Daniele Viganò <daniele@vigano.me>
2016-05-19 10:05:24 +0200

    Add a sleep in oq-server

--- oq-engine/oq-server.bat
+++ oq-engine/oq-server.bat
@@ -2,6 +2,10 @@
 setlocal
 set mypath=%~dp0
 set PYTHONPATH=pkgs
+REM Start the DbServer in background but within the same context
 start "OpenQuake DB server" /B "%COMMONPROGRAMFILES%\Python\2.7\python.exe" -m openquake.server.dbserver
+REM Make sure that the dbserver is up and running
+timeout /t 10 /nobreak > NUL
+REM Start the WebUI using django
 "%COMMONPROGRAMFILES%\Python\2.7\python.exe" -m openquake.server.manage runserver %*
 endlocal
