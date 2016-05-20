33055e1933c405405e49ce1cbec62391ae6f6e1c
Daniele Viganò <daniele@vigano.me>
2016-05-20 16:44:47 +0200

    Open the browser after the WebUI is started

--- oq-engine/oq-server.bat
+++ oq-engine/oq-server.bat
@@ -7,19 +7,28 @@ set PATH=%PATH%;%mypath%\python2.7
 REM Create the DB or update it
 python.exe -m openquake.server.db.upgrade_manager "%HOMEPATH%\db.sqlite3"
 
+echo Please wait ...
 REM Start the DbServer in background but within the same context
 start "OpenQuake DB server" /B python.exe -m openquake.server.dbserver
 
 REM Make sure that the dbserver is up and running
-echo Please wait ...
-if exist C:\Windows\System32\timeout.exe (
-    timeout /t 10 /nobreak > NUL
-) else (
-    REM Windows XP hack
-    ping 192.0.2.2 -n 1 -w 10000 > NUL 
-)
+call:sleep 5
 
 REM Start the WebUI using django
-python.exe -m openquake.server.manage runserver %*
+start "OpenQuake WebUI server" /B python.exe -m openquake.server.manage runserver %*
 
+REM Make sure that the dbserver is up and running
+call:sleep 2
+
+REM Start the browser
+start http://localhost:8000
+
+:sleep 
+if exist C:\Windows\System32\timeout.exe (
+    timeout /t %~1 /nobreak > NUL
+) else (
+    REM Windows XP hack
+    ping 192.0.2.2 -n %~1 -w 1000 > NUL 
+)
+exit /b 0
 endlocal
