d4ded6d43781e98b0e2c3c6a3bb0348b74257ef7
Daniele Viganò <daniele@vigano.me>
2016-05-20 15:48:40 +0200

    Fix doskey(s)

--- oq-engine/oq-console.bat
+++ oq-engine/oq-console.bat
@@ -4,8 +4,8 @@ set mypath=%~dp0
 set PATH=%PATH%;python2.7
 set PYTHONPATH=lib
 
-doskey oq-engine=python.exe -m openquake.engine.bin.openquake_cli %*
-doskey oq-lite=python.exe -m openquake.commonlib.commands %*
+doskey oq-engine=python.exe -m openquake.engine.bin.openquake_cli $*
+doskey oq-lite=python.exe -m openquake.commonlib.commands $*
 
 echo OpenQuake environment loaded
 cmd /k
