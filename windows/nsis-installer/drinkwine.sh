cf21683c1d5e46e49b56a0690fd11fe408ca1e0c
Daniele Viganò <daniele@vigano.me>
2016-10-06 18:23:30 +0200

    Add a script to run Win tests under Wine

---
+++ windows/nsis-installer/drinkwine.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+export WINEDEBUG=-all
+export WINEARCH="win32"
+
+if ! wine --version | grep -q wine-1.9; then
+    echo "Error: $0 requires Wine 1.9"
+    exit 1
+fi
+
+if [ -z $WINEPREFIX ]; then
+    echo "Error: please setup a custom WINEPREFIX first:"
+    echo "$ export WINEPREFIX=\"$HOME/oqtest\""
+    exit 1
+else
+    echo "Using WINEPREFIX=$WINEPREFIX"
+fi
+
+export PATH="$(pwd)/python2.7:$PATH"
+export PYTHONPATH="$(pwd)/lib"
+
+oq_root=$(pwd)
+nosetests="python.exe -m nose"
+
+cd lib/openquake
+for d in */; do
+    echo "Running $d tests"
+    cd $d
+    $nosetests -v -a '!slow'
+    cd ..
+done
