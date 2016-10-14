2c5063fc654d0f90adc1838cc971a74fe0d8594c
Daniele Viganò <daniele@vigano.me>
2016-10-14 15:12:38 +0200

    Check if webui is up in windows

---
+++ windows/nsis-installer/lib/checkifup.py
@@ -0,0 +1,14 @@
+import sys
+import time
+import urllib
+
+resp = ''
+
+if len(sys.argv) < 2:
+    raise ValueError('Missing argument host')
+
+host = str(sys.argv[1])
+
+while (resp != 200):
+    time.sleep(1)
+    resp = urllib.urlopen(host).getcode()
--- windows/nsis-installer/oq-server.bat
+++ windows/nsis-installer/oq-server.bat
@@ -4,7 +4,7 @@ set mypath=%~dp0
 set PATH=%mypath%\python2.7;%PATH%
 set PYTHONPATH=%mypath%\lib
 set OQ_SITE_CFG_PATH=%mypath%\openquake.cfg
-set OQ_HOST=127.0.0.1
+set OQ_HOST=localhost
 set OQ_PORT=8800
 
 echo Please wait ...
@@ -12,20 +12,10 @@ REM Start the WebUI using django
 start "OpenQuake WebUI server" /B python.exe -m openquake.commands webui start %OQ_HOST%:%OQ_PORT%
 
 REM Make sure that the dbserver is up and running
-call:sleep 10
+python lib\checkifup.py http://%OQ_HOST%:%OQ_PORT%
 
 REM Start the browser
-start http://localhost:%OQ_PORT%
+start http://%OQ_HOST%:%OQ_PORT%
 
 endlocal
 exit /b 0
-
-:sleep 
-setlocal
-if exist C:\Windows\System32\timeout.exe (
-    timeout /t %~1 /nobreak > NUL
-) else (
-    REM Windows XP hack
-    ping 192.0.2.2 -n %~1 -w 1000 > NUL 
-)
-endlocal
