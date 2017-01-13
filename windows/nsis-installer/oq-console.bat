e0e53507ea34769d31695854d7a042c1771cb423
Daniele Viganò <daniele@vigano.me>
2017-01-13 19:21:57 +0100

    Add an info about pip freeze

--- windows/nsis-installer/oq-console.bat
+++ windows/nsis-installer/oq-console.bat
@@ -10,6 +10,7 @@ doskey oq=python.exe -m openquake.commands.__main__ $*
 doskey oq-engine=python.exe -m openquake.commands.__main__ engine $*
 
 echo OpenQuake environment loaded
+echo To see versions of installed software run 'pip freeze'
 echo To run OpenQuake use 'oq' and 'oq engine'
 cmd /k
 
