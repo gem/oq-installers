e0b8cdab07e479d466dcd7bba8a4e0f9a73eceb9
Daniele Viganò <daniele@vigano.me>
2017-01-19 10:04:22 +0100

    Update unix installers

--- unix/opt/build-opt-unix.sh
+++ unix/opt/build-opt-unix.sh
@@ -94,43 +94,12 @@ cd build/src
 
 curl -LOz sed-4.2.2.tar.gz http://ftp.gnu.org/gnu/sed/sed-4.2.2.tar.gz
 curl -LOz openssl-1.0.2h.tar.gz https://www.openssl.org/source/openssl-1.0.2h.tar.gz
-curl -LOz Python-2.7.11.tar.xz https://www.python.org/ftp/python/2.7.11/Python-2.7.11.tar.xz
-curl -LOz hdf5-1.8.17.tar.gz http://www.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.8.17.tar.gz
+curl -LOz Python-2.7.11.tar.xz https://www.python.org/ftp/python/2.7.11/Python-2.7.12.tar.xz
 # FIXME Rtree is currently unsupported
 # curl -LOz 1.8.5.tar.gz https://github.com/libspatialindex/libspatialindex/archive/1.8.5.tar.gz
 curl -LOz get-pip.py https://bootstrap.pypa.io/get-pip.py
 
-if [ "$BUILD_OS" == "linux64" ]; then
-    curl -LOz 3.5.0.tar.gz https://github.com/libgeos/libgeos/archive/3.5.0.tar.gz
-fi
-
 cd ..
-
-cat <<EOF >> $OQ_PREFIX/requirements.txt
-pkgconfig==1.1.0
-Cython==0.23.4
-mock==1.3.0
-# h5py must be installed after everything else
-# to avoid SSL errors on MacOS X
-# h5py==2.6.0
-nose==1.3.7
-numpy==1.11.1
-scipy==0.17.1
-psutil==3.4.2
-shapely==1.5.13
-docutils==0.12
-decorator==4.0.10
-funcsigs==1.0.2
-pbr==1.8.0
-six==1.10.0
-futures==3.0.5
-django==1.8.7
-requests==2.9.1
-pyshp==1.2.3
-# FIXME Rtree is currently unsupported
-# and needs master to work with a custom LD_LIBRARY_PATH
-# https://github.com/Toblerity/rtree/archive/master.zip
-EOF
 
 cat <<EOF >> $OQ_PREFIX/env.sh
 PREFIX=$OQ_PREFIX
@@ -176,19 +145,10 @@ make -j $NPROC
 make install
 cd ..
 
-if $CLEANUP; then rm -Rf Python-2.7.11; fi
-tar xvf src/Python-2.7.11.tar.xz
+if $CLEANUP; then rm -Rf Python-2.7.12; fi
+tar xvf src/Python-2.7.12.tar.xz
 cd Python-2.7.11
 ./configure --prefix=$OQ_PREFIX --enable-unicode=ucs4
-make -j $NPROC
-make install
-cd ..
-
-if $CLEANUP; then rm -Rf hdf5-1.8.17; fi
-tar xvf src/hdf5-1.8.17.tar.gz
-cd hdf5-1.8.17
-export HDF5_DIR=$OQ_PREFIX
-./configure --prefix=$OQ_PREFIX
 make -j $NPROC
 make install
 cd ..
@@ -204,24 +164,12 @@ cd ..
 # make install
 # cd ..
 
-if [ "$BUILD_OS" == "linux64" ]; then
-    if $CLEANUP; then rm -Rf libgeos-3.5.0; fi
-    tar xvf src/3.5.0.tar.gz
-    cd libgeos-3.5.0
-    # Workaround for an autogen.sh bug
-    ./autogen.sh || true
-    ./autogen.sh
-    ./configure --prefix=$OQ_PREFIX
-    make -j $NPROC
-    make install
-    cd ..
-fi
-
 python src/get-pip.py
-python $(which pip) install -r $OQ_PREFIX/requirements.txt
-# h5py must be installed after everything else
-# to avoid SSL errors on MacOS X
-python $(which pip) install h5py==2.6.0
+if [ "$BUILD_OS" == "linux64" ]; then
+    python $(which pip) install -r $OQ_PREFIX/requirements-py27-linux64.txt
+elif [ "$BUILD_OS" == "macos" ]; then
+    python $(which pip) install -r $OQ_PREFIX/requirements-py27-macos.txt
+fi
 
 for g in hazardlib engine;
 do 
@@ -229,7 +177,7 @@ do
     git clone --depth=1 -b $OQ_BRANCH https://github.com/gem/oq-${g}.git
     cd oq-${g}
     declare OQ_$(echo $g | tr '[:lower:]' '[:upper:]')_DEV=$(git rev-parse --short HEAD)
-    python setup.py install
+    pip install .
     cd ..
 done
 
