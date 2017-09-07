cff1a1c4b35ff618656f7ab0ce80157cd087e141
Daniele Viganò <daniele@vigano.me>
2017-09-07 17:24:56 +0200

    Add docker container for nighlty win zip

--- installers/windows/nsis/docker/build-nightly.sh
+++ installers/windows/nsis/docker/build-nightly.sh
@@ -5,16 +5,9 @@ if [ $GEM_SET_DEBUG ]; then
 fi
 set -e
 
-if [ $GEM_SET_BRANCH ]; then
-    OQ_BRANCH=$GEM_SET_BRANCH
-else
-    OQ_BRANCH=master
-fi
-
 # Default software distribution
 PY="2.7.13"
 PY_MSI="python-$PY.amd64.msi"
-
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/../src && pwd
@@ -33,11 +26,10 @@ rm -Rf ../demos/*
 ## not packaged in the python packages
 # pip wheel --no-deps https://github.com/gem/oq-engine/archive/master.zip
 
-for app in oq-engine oq-platform-standalone oq-platform-ipt oq-platform-taxtweb oq-platform-taxonomy; do
+for app in oq-engine; do
     if [ ! -d $app ]; then
         git clone -q -b $OQ_BRANCH --depth=1 https://github.com/gem/${app}.git
     fi
-    wine pip -q wheel --disable-pip-version-check --no-deps ./${app}
 done
 
 # Extract Python, to be included in the installation
@@ -49,20 +41,13 @@ wine msiexec /a $PY_MSI /qb TARGETDIR=../python-dist/python2.7
 
 # Extract wheels to be included in the installation
 echo "Extracting python wheels"
-wine pip -q install --disable-pip-version-check --force-reinstall --ignore-installed --upgrade --no-deps --no-index --prefix ../python-dist py/*.whl py27/*.whl openquake.*.whl oq_platform*.whl
+wine pip -q install --disable-pip-version-check --force-reinstall --ignore-installed --upgrade --no-deps --no-index --prefix ../python-dist py/*.whl py27/*.whl
 
+ZIP="OpenQuake_Engine_win64_dev$(date '+%y%m%d%H%M').zip"
+
+echo "Generating zip archive"
+zip -qr ../${ZIP} oq-engine
+cd ../python-dist
+zip -qr ../${ZIP} Lib python2.7
 cd ..
-
-# Get the demo and the README
-cp -r src/oq-engine/demos .
-src/oq-engine/helpers/zipdemos.sh $(pwd)/demos
-
-python -m markdown src/oq-engine/README.md > README.html
-
-# Get a copy of the OQ manual if not yet available
-if [ ! -f OpenQuake\ manual.pdf ]; then
-    wget -O- https://ci.openquake.org/job/builders/job/pdf-builder/lastSuccessfulBuild/artifact/oq-engine/doc/manual/oq-manual.pdf > OpenQuake\ manual.pdf
-fi
-
-echo "Generating NSIS installer"
-wine ${HOME}/.wine/drive_c/Program\ Files\ \(x86\)/NSIS/makensis /V4 installer.nsi
+zip -qr ${ZIP} *.bat
