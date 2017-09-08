d1573597aff61932bd7116f597f548db39aac0ca
Daniele Viganò <daniele@vigano.me>
2017-09-08 07:59:54 +0200

    Improvements and cleanup

--- installers/windows/nsis/docker/build-nightly.sh
+++ installers/windows/nsis/docker/build-nightly.sh
@@ -5,31 +5,40 @@ if [ $GEM_SET_DEBUG ]; then
 fi
 set -e
 
+if [ $GEM_SET_BRANCH ]; then
+    OQ_BRANCH=$GEM_SET_BRANCH
+else
+    OQ_BRANCH=master
+fi
+
 # Default software distribution
 PY="2.7.13"
 PY_MSI="python-$PY.amd64.msi"
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR/../src && pwd
-
-if [ ! -d py -o ! -d py27 ]; then
-    echo "Please download python dependencies first."
-    exit 1
-fi
+TMPDIR=$(mktemp -d)
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+cd $DIR && pwd
 
 # Cleanup
-rm -Rf ../python-dist/python2.7/*
-rm -Rf ../python-dist/Lib/*
-rm -Rf ../demos/*
+rm -Rf python-dist/python2.7/*
+rm -Rf python-dist/Lib/*
+rm -Rf demos/*
 
 ## This is an alternative method that we cannot use because we need extra data
 ## not packaged in the python packages
 # pip wheel --no-deps https://github.com/gem/oq-engine/archive/master.zip
 
+cd src
+if [ ! -d py -o ! -d py27 ]; then
+    echo "Please download python dependencies first."
+    exit 1
+fi
+
 for app in oq-engine; do
     if [ ! -d $app ]; then
         git clone -q -b $OQ_BRANCH --depth=1 https://github.com/gem/${app}.git
     fi
+    git -C $app archive --prefix=$app/ HEAD | tar -C $TMPDIR -xf -
 done
 
 # Extract Python, to be included in the installation
@@ -46,12 +55,12 @@ wine pip -q install --disable-pip-version-check --force-reinstall --ignore-insta
 ZIP="OpenQuake_Engine_win64_dev$(date '+%y%m%d%H%M').zip"
 OQPYPATH='s/PYTHONPATH=%mypath%\\lib\\site-packages/PYTHONPATH=%mypath%\\oq-engine;%mypath%\\lib\\site-packages/g'
 
+cd $TMPDIR
 echo "Generating zip archive"
 for b in oq-console.bat oq-server.bat; do
-    sed "$OQPYPATH" ../${b} > $b
-    zip -qr ../${ZIP} $b
-    rm $b
+    sed "$OQPYPATH" ${DIR}/${b} > $b
+    zip -qr $DIR/${ZIP} $b
 done
-zip -qr ../${ZIP} oq-engine
-cd ../python-dist
-zip -qr ../${ZIP} Lib python2.7
+zip -qr $DIR/$ZIP oq-engine
+cd $DIR/python-dist
+zip -qr $DIR/$ZIP Lib python2.7
--- installers/windows/nsis/docker/build.sh
+++ installers/windows/nsis/docker/build.sh
@@ -16,8 +16,8 @@ PY="2.7.13"
 PY_MSI="python-$PY.amd64.msi"
 
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR/../src && pwd
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+cd $DIR && pwd
 
 if [ ! -d py -o ! -d py27 ]; then
     echo "Please download python dependencies first."
@@ -25,14 +25,15 @@ if [ ! -d py -o ! -d py27 ]; then
 fi
 
 # Cleanup
-rm -Rf ../python-dist/python2.7/*
-rm -Rf ../python-dist/Lib/*
-rm -Rf ../demos/*
+rm -Rf python-dist/python2.7/*
+rm -Rf python-dist/Lib/*
+rm -Rf demos/*
 
 ## This is an alternative method that we cannot use because we need extra data
 ## not packaged in the python packages
 # pip wheel --no-deps https://github.com/gem/oq-engine/archive/master.zip
 
+cd src
 for app in oq-engine oq-platform-standalone oq-platform-ipt oq-platform-taxtweb oq-platform-taxonomy; do
     if [ ! -d $app ]; then
         git clone -q -b $OQ_BRANCH --depth=1 https://github.com/gem/${app}.git
@@ -51,7 +52,7 @@ wine msiexec /a $PY_MSI /qb TARGETDIR=../python-dist/python2.7
 echo "Extracting python wheels"
 wine pip -q install --disable-pip-version-check --force-reinstall --ignore-installed --upgrade --no-deps --no-index --prefix ../python-dist py/*.whl py27/*.whl openquake.*.whl oq_platform*.whl
 
-cd ..
+cd $DIR
 
 # Get the demo and the README
 cp -r src/oq-engine/demos .
