68e8d90415106432532dd528a44e604222d9aba0
Daniele Viganò <daniele@vigano.me>
2017-09-12 14:12:46 +0200

    Fix a typo [skip ci]

--- installers/unix/opt/install.sh
+++ installers/unix/opt/install.sh
@@ -114,7 +114,7 @@ cp -R src/{README.md,LICENSE,demos,doc} $FDEST/share
 ## Tools installation
 # A question Y/N is prompt to the user: if answer is Y tools (IPT...) will be installed together with
 # the OpenQuake Engine, otherwise with N only the Engine is installed and configured.
-# To allow unattended installationis a "force" flag can be passed, either force Y (--yes) or force N (--no)
+# To allow unattended installations a "force" flag can be passed, either force Y (--yes) or force N (--no)
 if [ -z $FORCE ]; then
     PROMPT="Do you want to install the OpenQuake Tools (IPT, TaxtWeb, Taxonomy Glossary)? [Y/n]: "
     read -e -p "$PROMPT" TOOLS
