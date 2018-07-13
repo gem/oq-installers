2e0f3115e11280038f27bac6a5b51fcc09778382
Daniele Viganò <daniele@vigano.me>
2018-07-13 10:56:59 +0200

    Move windows installer to Python 3.6

--- installers/windows/nsis/oq-console.bat
+++ installers/windows/nsis/oq-console.bat
@@ -1,13 +1,13 @@
 @echo off
 setlocal
 set mypath=%~dp0
-set PATH=%mypath%\python3.5;%mypath%\python3.5\Scripts;%PATH%
+set PATH=%mypath%\python3.6;%mypath%\python3.6\Scripts;%PATH%
 
-if not exist python3.5\pycached (
+if not exist python3.6\pycached (
    echo Building python cache. This may take a while.
    echo Please wait ...
    python.exe -m compileall -qq .
-   copy /y nul python3.5\pycached >nul
+   copy /y nul python3.6\pycached >nul
 )
 
 echo OpenQuake environment loaded
--- installers/windows/nsis/oq-server.bat
+++ installers/windows/nsis/oq-server.bat
@@ -1,15 +1,15 @@
 @echo off
 setlocal
 set mypath=%~dp0
-set PATH=%mypath%\python3.5;%mypath%\python3.5\Scripts;%PATH%
+set PATH=%mypath%\python3.6;%mypath%\python3.6\Scripts;%PATH%
 set OQ_HOST=localhost
 set OQ_PORT=8800
 
-if not exist python3.5\pycached (
+if not exist python3.6\pycached (
    echo Building python cache. This may take a while.
    echo Please wait ...
    python.exe -m compileall -qq .
-   copy /y nul python3.5\pycached >nul
+   copy /y nul python3.6\pycached >nul
 )
 
 echo Starting the server.
