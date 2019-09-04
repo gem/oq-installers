1773745f36d12ed594173f42299ef99d1cdbceb7
Daniele Viganò <daniele@vigano.me>
2019-09-04 14:29:27 +0200

    Add some comments about GDAL without numpy

--- wheels/builders/gdal.sh
+++ wheels/builders/gdal.sh
@@ -82,6 +82,7 @@ cp $MYDIR/gdal/gdalinit.py /tmp/src/gdal-2.4.1/swig/python/osgeo/__init__.py
 
 cd  /tmp/src/gdal-2.4.1/swig/python
 
+# numpy is disabled to avoid recompiling GDAL every time numpy is updated
 # get numpy==1.16.5
 build .
 
