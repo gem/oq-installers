962f0ff36a3eb0be03a14672f6ee4c30845dbe59
Antonio Ettorre <antonio.ettorre@globalquakemodel.org>
2020-08-18 16:30:17 +0200

    set >= for numpy version on setup.py

--- wheels/builders/gdal/setup.py
+++ wheels/builders/gdal/setup.py
@@ -7,7 +7,7 @@
 # Howard Butler hobu.inc@gmail.com
 
 
-gdal_version = '2.4.1.post1'
+gdal_version = '2.4.2'
 
 import sys
 import shutil
@@ -331,7 +331,8 @@ if HAVE_SETUPTOOLS:
            exclude_package_data = exclude_package_data,
            cmdclass={'build_ext':gdal_ext},
            ext_modules = ext_modules,
-           install_requires=['numpy=={}'.format(numpy.__version__)],
+           #install_requires=['numpy=={}'.format(numpy.__version__)],
+           install_requires=['numpy>={}'.format(numpy.__version__)],
            package_data={'osgeo': ['gdal_data/*', 'proj_data/*']},
            **extra )
 else:
